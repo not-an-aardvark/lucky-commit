00000002d54ed470719ca50db78d349ea5578c95
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-04-09 01:36:24 -0400

    Use whitespace instead of gibberish characters
    This makes the commit messages a bit longer since fewer characters are available, but the overall effect is less visible.

--- README.md
+++ README.md
@@ -8,18 +8,18 @@ With this simple command, you can change the start of your git commit hashes to 
 
 ```bash
 $ git log
-1f6383a (HEAD -> master) Some commit (take note of the hash on the left)
+1f6383a (HEAD -> master) Some commit
 $ npm install -g lucky-commit
 $ lucky-commit
 $ git log
-0000000 (HEAD -> master) Some commit (take note of the hash on the left)
+0000000 (HEAD -> master) Some commit
 ```
 
 As a demonstration, see the latest commit in this repository.
 
 ## How?
 
-lucky-commit amends your commit messages by adding a few characters of gibberish to the end, and keeps hashing new commits until it gets the right value. By default, it will keep searching until it finds a hash starting with "0000000", but this can be changed by simply passing the desired hash as a parameter.
+lucky-commit amends your commit messages by adding a few characters of various types of whitespace to the end, and keeps hashing new messages until it gets the right value. By default, it will keep searching until it finds a hash starting with "0000000", but this can be changed by simply passing the desired hash as a parameter.
 
 ```bash
 $ lucky-commit 1010101
@@ -27,7 +27,7 @@ $ git log
 1010101 (HEAD -> master) Some commit (take note of the hash on the left)
 ```
 
-On average, this requires the computation of 16^7 sha1 hashes, which takes about 20 minutes on my laptop.
+On average, this requires the computation of 16<sup>7</sup> sha1 hashes, which takes about 20 minutes on my laptop.
 
 ## Why?
 
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 'use strict';
-const exec = require('child_process').execSync;
+const exec = command => require('child_process').execSync(command).toString();
 const desiredString = process.argv[2] || '0000000';
-if (desiredString.length > 40 || !/^[0-9a-f]*$/.test(desiredString)) {
-  throw 'Error: Invalid input provided. (If an input is provided, it must be a hex string.)';
+if (!/^[0-9a-f]{1,40}$/.test(desiredString)) {
+  throw new TypeError('Invalid input provided. (If an input is provided, it must be a hex string.)');
 }
-const lastCommit = exec('git cat-file commit head').toString();
-const previousDate = lastCommit.split('\n')[2].match(/\d*? [-|+]\d{4}$/)[0];
-let currentStr = lastCommit;
-for (let numAttempts = 0; !require('crypto').createHash('sha1').update(currentStr).digest('hex').startsWith(desiredString); numAttempts++) {
-  const strWithNewMessage = `${lastCommit.slice(0, -1)}\n\n${numAttempts.toString(36)}\n`;
+const lastCommit = exec('git cat-file commit head');
+const previousDate = exec('git --no-pager show -s --oneline --format="%cd" head');
+const previousMessage = exec('git --no-pager show -s --oneline --format="%B" head').slice(0, -2);
+let currentStr = lastCommit, counter = 0, whitespace;
+do {
+  whitespace = (++counter).toString(2).replace(/0/g, ' ').replace(/1/g, '\t');
+  const strWithNewMessage = `${lastCommit.slice(0, -1)}${whitespace}\n`;
   currentStr = `commit ${strWithNewMessage.length}\u0000${strWithNewMessage}`;
-}
-exec(`GIT_COMMITTER_DATE="${previousDate}" git commit --amend -m '${currentStr.split('\n').slice(4, -1).join('\n').replace(/'/g, `'"'"'`)}'`);
+} while (!require('crypto').createHash('sha1').update(currentStr).digest('hex').startsWith(desiredString))
+exec(`GIT_COMMITTER_DATE="${previousDate}" git commit --amend --cleanup=verbatim -m '${previousMessage.replace(/'/g, `'"'"'`)}${whitespace}'`);
