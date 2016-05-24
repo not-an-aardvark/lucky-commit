00000006232e1c3c42436af6a7a5258ebd3b6618
not-an-aardvark <not-an-aardvark@users.noreply.github.com>
2016-05-24 04:15:49 -0400

    Use \x00 instead of \u0000 for null character

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function luckyCommit (desiredString) {
   do {
     whitespace = (++counter).toString(2).replace(/0/g, ' ').replace(/1/g, '\t');
     const strWithNewMessage = lastCommit.slice(0, -1) + whitespace + '\n';
-    currentStr = 'commit ' + strWithNewMessage.length + '\u0000' + strWithNewMessage;
+    currentStr = 'commit ' + strWithNewMessage.length + '\x00' + strWithNewMessage;
   } while (!require('crypto').createHash('sha1').update(currentStr).digest('hex').startsWith(desiredString))
   exec('GIT_COMMITTER_DATE="' + previousDate + '" git commit --amend --cleanup=verbatim -m \'' + previousMessage.replace(/'/g, "'\"'\"'") + whitespace + "'");
 }
