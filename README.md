000000024690beab7abfeba60c2c8d9aefff5062
Teddy Katz <teddy.katz@gmail.com>
2021-01-30 02:39:05 -0500

    Add a less noisy benchmarking option

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ With this simple tool, you can change the start of your git commit hashes to wha
 ```bash
 $ git log
 1f6383a Some commit
-$ lucky-commit
+$ lucky_commit
 $ git log
 0000000 Some commit
 ```
@@ -21,7 +21,7 @@ As a demonstration, see the latest commit in this repository.
 `lucky-commit` amends your commit messages by adding a few characters of various types of whitespace, and keeps trying new messages until it finds a good hash. By default, it will keep searching until it finds a hash starting with "0000000", but this can be changed by simply passing the desired hash as an argument.
 
 ```bash
-$ lucky-commit 1010101
+$ lucky_commit 1010101
 $ git log
 1010101 Some commit
 ```
@@ -61,5 +61,7 @@ For non-GPG-signed commits, `lucky-commit` adds its whitespace to a 64-byte-alig
 ```
 (16^7 hashes) / (6400000 hashes/s/core) / (2 physical cores) = 21 seconds
 ```
+
+You can estimate the average time for your computer by running `time lucky_commit --benchmark`, or by following the instructions in the [benchmarking test branch](https://github.com/not-an-aardvark/lucky-commit/tree/benchmark-branch).
 
 For GPG-signed commits, the commit message is part of the signed payload, so `lucky-commit` can't edit the commit message without making the signature invalid. Instead, it adds its whitespace to the end of the signature itself. Since the signature precedes the commit message in git's commit encoding, this requires `lucky-commit` to do more work on each attempt (it can't cache the SHA1 buffer state as effectively, and it needs to rehash the commit message every time). As a result, the performance for GPG-signed commits depends on the length of the commit message. As a rule of thumb, multiply the number above for non-GPG-signed commits by `1 + ceiling(commit message length / 64 bytes)` to get an estimated wait time for GPG-signed commits.
--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,11 @@ struct ProcessedCommit {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() == 2 && args[1] == "--benchmark" {
+        run_single_core_benchmark();
+        return;
+    }
+
     match args.len() {
         1 => run_lucky_commit(&parse_prefix("0000000").unwrap()),
         2 => match parse_prefix(&args[1]) {
@@ -60,7 +65,7 @@ fn main() {
 }
 
 fn print_usage_and_exit() -> ! {
-    fail_with_message("Usage: lucky-commit [commit-hash-prefix]")
+    fail_with_message("Usage: lucky_commit [commit-hash-prefix]")
 }
 
 fn fail_with_message(message: &str) -> ! {
@@ -347,6 +352,49 @@ fn to_hex_string(hash: &[u8]) -> String {
     hash.iter()
         .map(|byte| format!("{:02x}", *byte))
         .collect::<String>()
+}
+
+fn run_single_core_benchmark() {
+    // Runs a benchmark for performance testing. Using a single core, this does a constant
+    // hash search. This benchmark should take roughly the same amount of time as running
+    // `lucky_commit` with no arguments, but the performance should be much more consistent.
+    // Caveats:
+    // * Assumes perfect parallelization by scaling the workload down when there are multiple
+    //   CPUs. While hash searching is perfectly parallelizable in theory, this benchmark might
+    //   fail to catch performance bugs resulting from contention.
+    // * Assumes that CPU utilization would remain the same when adding threads (this might
+    //   not be the case if the other CPUs are being used for something else)
+    // * Might overestimate the available cache space, since in reality multiple threads would
+    //   be sharing the cache.
+    // * The benchmark uses an unusually long desired prefix to make it implausible that it ends
+    //   early. While this shouldn't result in substantially more instructions executed, in theory
+    //   it could have an effect on things like alignment and the cache.
+    // * The benchmark doesn't spawn any git commands or interact with the filesystem, whereas
+    // * a real run does a ~single-digit number of filesystem operations.
+    //
+    // To use: run `time target/release/lucky_commit --benchmark` and look at the user time.
+    // The observed standard deviation for this benchmark is somewhere around 0.05 seconds.
+    //
+    // For a more end-to-end benchmark without the above caveats and with more noise, see the
+    // `benchmark-branch` branch. The observed standard deviation for that benchmark is somewhere
+    // around 0.5 seconds.
+    assert_eq!(
+        None,
+        iterate_for_match(&SearchParams {
+            current_commit: "tree 6f4e79123e206448f80ec73b9a53e07eb0784fef\n\
+                                     author Foo Bar <foo@example.com> 1611912738 -0500\n\
+                                     committer Foo Bar <foo@example.com> 1611912738 -0500\n\
+                                     \n\
+                                     Test commit for benchmarking performance changes\n"
+                .to_owned(),
+            desired_prefix: HashPrefix {
+                data: vec![0; 19],
+                half_byte: Some(0x0)
+            },
+            counter_range: 1..((1 << 28) / num_cpus::get_physical() as u64)
+        })
+    );
+    return;
 }
 
 #[cfg(test)]
