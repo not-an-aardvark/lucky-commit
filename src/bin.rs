00000001a3ffc39eb1f3553456d8c3116a051e4f
Teddy Katz <teddy.katz@gmail.com>
2021-02-01 21:05:56 -0500

    Split package into separate library and binary

---
+++ src/bin.rs
@@ -0,0 +1,392 @@
+use lucky_commit_lib::{iterate_for_match, HashMatch, HashPrefix, SearchParams, SHA1_BYTE_LENGTH};
+use std::env;
+use std::io;
+use std::io::Write;
+use std::ops;
+use std::process::{exit, Command, Stdio};
+use std::sync::mpsc;
+use std::thread;
+use std::u8;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 && args[1] == "--benchmark" {
+        run_single_core_benchmark();
+        return;
+    }
+
+    match args.len() {
+        1 => run_lucky_commit(&parse_prefix("0000000").unwrap()),
+        2 => match parse_prefix(&args[1]) {
+            Some(prefix) => run_lucky_commit(&prefix),
+            None => print_usage_and_exit(),
+        },
+        _ => print_usage_and_exit(),
+    }
+}
+
+fn print_usage_and_exit() -> ! {
+    fail_with_message("Usage: lucky_commit [commit-hash-prefix]")
+}
+
+fn fail_with_message(message: &str) -> ! {
+    eprintln!("{}", message);
+    exit(1)
+}
+
+fn parse_prefix(prefix: &str) -> Option<HashPrefix> {
+    if prefix.len() > SHA1_BYTE_LENGTH * 2 {
+        return None;
+    }
+
+    let mut data = Vec::new();
+    for index in 0..(prefix.len() / 2) {
+        match u8::from_str_radix(&prefix[2 * index..2 * index + 2], 16) {
+            Ok(value) => data.push(value),
+            Err(_) => return None,
+        }
+    }
+
+    Some(HashPrefix {
+        data,
+        half_byte: if prefix.len() % 2 == 1 {
+            match u8::from_str_radix(&prefix[prefix.len() - 1..], 16) {
+                Ok(value) => Some(value << 4),
+                Err(_) => return None,
+            }
+        } else {
+            None
+        },
+    })
+}
+
+fn run_lucky_commit(desired_prefix: &HashPrefix) {
+    let current_commit_bytes = run_command("git", &["cat-file", "commit", "HEAD"]);
+    let current_commit =
+        &String::from_utf8(current_commit_bytes).expect("Git commit contains invalid utf8");
+
+    match find_match(current_commit, desired_prefix) {
+        Some(hash_match) => {
+            create_git_commit(&hash_match)
+                .expect("Found a commit, but failed to write it to the git object database.");
+            git_reset_to_hash(&hash_match.hash);
+        }
+        None => fail_with_message(
+            "Sorry, failed to find a commit matching the givenprefix despite searching over \
+             281 trillion possible commits. Hopefully you haven't just been sitting here \
+             waiting the whole time.",
+        ),
+    }
+}
+
+fn run_command(command: &str, args: &[&str]) -> Vec<u8> {
+    let output = Command::new(command)
+        .args(args)
+        .stderr(Stdio::inherit())
+        .output()
+        .unwrap_or_else(|_| {
+            panic!(
+                "Failed to spawn command `{}` with args `{:?}`",
+                command, args
+            )
+        });
+
+    if !output.status.success() {
+        panic!(
+            "Command finished with non-zero exit code: {} {:?}",
+            command, args
+        );
+    }
+
+    output.stdout
+}
+
+fn find_match(current_commit: &str, desired_prefix: &HashPrefix) -> Option<HashMatch> {
+    let num_threads = num_cpus::get_physical();
+    let (shared_sender, receiver) = mpsc::channel();
+    let counter_ranges = split_range(0, 1u64 << 48, num_threads);
+
+    for counter_range in counter_ranges {
+        spawn_hash_searcher(
+            shared_sender.clone(),
+            SearchParams {
+                current_commit: current_commit.to_owned(),
+                desired_prefix: desired_prefix.clone(),
+                counter_range: counter_range.clone(),
+            },
+        );
+    }
+
+    for _ in 0..num_threads {
+        let result = receiver.recv().unwrap();
+        if result.is_some() {
+            return result;
+        }
+    }
+
+    None
+}
+
+fn spawn_hash_searcher(result_sender: mpsc::Sender<Option<HashMatch>>, params: SearchParams) {
+    thread::spawn(move || {
+        /*
+         * If an error occurs when sending, then the receiver has already received
+         * a match from another thread, so ignore the error.
+         */
+        let _ = result_sender.send(iterate_for_match(&params));
+    });
+}
+
+fn split_range(min: u64, max: u64, num_segments: usize) -> Vec<ops::Range<u64>> {
+    let segment_size = (max - min) / (num_segments as u64);
+
+    let mut segments = Vec::new();
+    let mut last_range_end = 0;
+    for _ in 0..num_segments {
+        segments.push(last_range_end..last_range_end + segment_size);
+        last_range_end += segment_size;
+    }
+
+    segments
+}
+
+fn create_git_commit(search_result: &HashMatch) -> io::Result<()> {
+    assert!(&search_result.raw_object[0..7] == b"commit ");
+    let commit_start_index = search_result
+        .raw_object
+        .iter()
+        .position(|byte| *byte == 0)
+        .expect("No null character found in constructed raw git object?")
+        + 1;
+
+    let mut git_hash_object_child = Command::new("git")
+        .args(&["hash-object", "-t", "commit", "-w", "--stdin"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::inherit())
+        .spawn()?;
+
+    git_hash_object_child
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(&search_result.raw_object[commit_start_index..])?;
+    let output = git_hash_object_child.wait_with_output()?;
+
+    if !output.status.success() {
+        panic!("Found a commit, but failed to write it to the git object database.");
+    }
+    let git_hash_output =
+        String::from_utf8(output.stdout).expect("Git produced a hash containing invalid utf8?");
+    assert!(
+        git_hash_output.trim_end() == to_hex_string(&search_result.hash),
+        "Found a commit ({}), but git unexpectedly computed a different hash for it ({})",
+        to_hex_string(&search_result.hash),
+        git_hash_output.trim_end(),
+    );
+    Ok(())
+}
+
+fn git_reset_to_hash(hash: &[u8; SHA1_BYTE_LENGTH]) {
+    run_command("git", &["reset", &to_hex_string(hash)]);
+}
+
+fn to_hex_string(hash: &[u8]) -> String {
+    hash.iter()
+        .map(|byte| format!("{:02x}", *byte))
+        .collect::<String>()
+}
+
+fn run_single_core_benchmark() {
+    // Runs a benchmark for performance testing. Using a single core, this does a constant
+    // hash search. This benchmark should take roughly the same amount of time as running
+    // `lucky_commit` with no arguments, but the performance should be much more consistent.
+    // Caveats:
+    // * Assumes perfect parallelization by scaling the workload down when there are multiple
+    //   CPUs. While hash searching is perfectly parallelizable in theory, this benchmark might
+    //   fail to catch performance bugs resulting from contention.
+    // * Assumes that CPU utilization would remain the same when adding threads (this might
+    //   not be the case if the other CPUs are being used for something else)
+    // * Might overestimate the available cache space, since in reality multiple threads would
+    //   be sharing the cache.
+    // * The benchmark uses an unusually long desired prefix to make it implausible that it ends
+    //   early. While this shouldn't result in substantially more instructions executed, in theory
+    //   it could have an effect on things like alignment and the cache.
+    // * The benchmark doesn't spawn any git commands or interact with the filesystem, whereas
+    // * a real run does a ~single-digit number of filesystem operations.
+    //
+    // To use: run `time target/release/lucky_commit --benchmark` and look at the user time.
+    // The observed standard deviation for this benchmark is somewhere around 0.05 seconds.
+    //
+    // For a more end-to-end benchmark without the above caveats and with more noise, see the
+    // `benchmark-branch` branch. The observed standard deviation for that benchmark is somewhere
+    // around 0.5 seconds.
+    assert_eq!(
+        None,
+        iterate_for_match(&SearchParams {
+            current_commit: "\
+                    tree 6f4e79123e206448f80ec73b9a53e07eb0784fef\n\
+                    author Foo Bar <foo@example.com> 1611912738 -0500\n\
+                    committer Foo Bar <foo@example.com> 1611912738 -0500\n\
+                    \n\
+                    Test commit for benchmarking performance changes\n"
+                .to_owned(),
+            desired_prefix: HashPrefix {
+                data: vec![0; 19],
+                half_byte: Some(0x0)
+            },
+            counter_range: 1..((1 << 28) / num_cpus::get_physical() as u64)
+        })
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_prefix_empty() {
+        assert_eq!(
+            Some(HashPrefix {
+                data: Vec::new(),
+                half_byte: None
+            }),
+            parse_prefix("")
+        )
+    }
+
+    #[test]
+    fn parse_prefix_single_char() {
+        assert_eq!(
+            Some(HashPrefix {
+                data: Vec::new(),
+                half_byte: Some(0xa0)
+            }),
+            parse_prefix("a")
+        )
+    }
+
+    #[test]
+    fn parse_prefix_even_chars() {
+        assert_eq!(
+            Some(HashPrefix {
+                data: vec![0xab, 0xcd, 0xef],
+                half_byte: None
+            }),
+            parse_prefix("abcdef")
+        )
+    }
+
+    #[test]
+    fn parse_prefix_odd_chars() {
+        assert_eq!(
+            Some(HashPrefix {
+                data: vec![0xab, 0xcd, 0xef],
+                half_byte: Some(0x50)
+            }),
+            parse_prefix("abcdef5")
+        )
+    }
+
+    #[test]
+    fn parse_prefix_capital_letters() {
+        assert_eq!(
+            Some(HashPrefix {
+                data: vec![0xab, 0xcd, 0xef],
+                half_byte: Some(0xb0)
+            }),
+            parse_prefix("ABCDEFB")
+        )
+    }
+
+    #[test]
+    fn parse_prefix_invalid_even_chars() {
+        assert_eq!(None, parse_prefix("abcdgeb"))
+    }
+
+    #[test]
+    fn parse_prefix_invalid_odd_char() {
+        assert_eq!(None, parse_prefix("abcdefg"))
+    }
+
+    #[test]
+    fn parse_prefix_exact_length_match() {
+        assert_eq!(
+            Some(HashPrefix {
+                data: vec![
+                    0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12,
+                    0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78
+                ],
+                half_byte: None
+            }),
+            parse_prefix("1234567812345678123456781234567812345678")
+        )
+    }
+
+    #[test]
+    fn parse_prefix_too_long_with_half_byte() {
+        assert_eq!(
+            None,
+            parse_prefix("12345678123456781234567812345678123456781")
+        )
+    }
+
+    #[test]
+    fn parse_prefix_too_many_full_bytes() {
+        assert_eq!(
+            None,
+            parse_prefix("123456781234567812345678123456781234567812")
+        )
+    }
+
+    #[test]
+    fn split_range_u32_one_segment() {
+        assert_eq!(vec![0..1u64 << 32], split_range(0, 1u64 << 32, 1));
+    }
+
+    #[test]
+    fn split_range_u32_multiple_segments() {
+        let range_max = 1u64 << 32;
+        assert_eq!(
+            vec![
+                0..range_max / 5,
+                range_max / 5..2 * range_max / 5,
+                2 * range_max / 5..3 * range_max / 5,
+                3 * range_max / 5..4 * range_max / 5,
+                4 * range_max / 5..range_max - 1
+            ],
+            split_range(0, range_max, 5)
+        );
+    }
+
+    #[test]
+    fn split_range_u64_one_segment() {
+        assert_eq!(vec![0..u64::MAX], split_range(0, u64::MAX, 1));
+    }
+
+    #[test]
+    fn split_range_u64_multiple_segments() {
+        let range_max = u64::MAX;
+        assert_eq!(
+            vec![
+                0..range_max / 5,
+                range_max / 5..range_max / 5 * 2,
+                range_max / 5 * 2..range_max / 5 * 3,
+                range_max / 5 * 3..range_max / 5 * 4,
+                range_max / 5 * 4..range_max
+            ],
+            split_range(0, range_max, 5)
+        );
+    }
+
+    #[test]
+    fn to_hex_string_basic() {
+        assert_eq!("00", to_hex_string(&[0]));
+    }
+
+    #[test]
+    fn to_hex_string_multichar() {
+        assert_eq!("00ff14", to_hex_string(&[0, 255, 20]));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,32 @@
 use sha1::{digest::FixedOutputDirty, Digest, Sha1};
-
-use std::env;
-use std::io;
-use std::io::Write;
 use std::ops;
-use std::process::{exit, Command, Stdio};
-use std::str;
-use std::sync::mpsc;
-use std::thread;
-use std::u64;
-use std::u8;
 
-const SHA1_BYTE_LENGTH: usize = 20;
+pub const SHA1_BYTE_LENGTH: usize = 20;
 
 // See the comment in `process_commit` for the commit and padding layout.
 const DYNAMIC_PADDING_LENGTH: usize = 48;
 
 #[derive(Debug, PartialEq, Clone)]
-struct HashPrefix {
-    data: Vec<u8>,
-    half_byte: Option<u8>,
+pub struct HashPrefix {
+    //
+    pub data: Vec<u8>,
+
+    // If the desired prefix has an odd number of characters when represented
+    // as a hex string, the last character should be specified in `half_byte`.
+    // Only the most significant byte is used.
+    pub half_byte: Option<u8>,
 }
 
-struct SearchParams {
-    current_commit: String,
-    desired_prefix: HashPrefix,
-    counter_range: ops::Range<u64>,
+pub struct SearchParams {
+    pub current_commit: String,
+    pub desired_prefix: HashPrefix,
+    pub counter_range: ops::Range<u64>,
 }
 
 #[derive(Debug, PartialEq)]
-struct HashMatch {
-    raw_object: Vec<u8>,
-    hash: [u8; SHA1_BYTE_LENGTH],
+pub struct HashMatch {
+    pub raw_object: Vec<u8>,
+    pub hash: [u8; SHA1_BYTE_LENGTH],
 }
 
 #[derive(Debug, PartialEq)]
@@ -40,150 +35,23 @@ struct ProcessedCommit {
     dynamic_padding_start_index: usize,
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() == 2 && args[1] == "--benchmark" {
-        run_single_core_benchmark();
-        return;
-    }
-
-    match args.len() {
-        1 => run_lucky_commit(&parse_prefix("0000000").unwrap()),
-        2 => match parse_prefix(&args[1]) {
-            Some(prefix) => run_lucky_commit(&prefix),
-            None => print_usage_and_exit(),
-        },
-        _ => print_usage_and_exit(),
-    }
-}
-
-fn print_usage_and_exit() -> ! {
-    fail_with_message("Usage: lucky_commit [commit-hash-prefix]")
-}
-
-fn fail_with_message(message: &str) -> ! {
-    eprintln!("{}", message);
-    exit(1)
-}
-
-fn parse_prefix(prefix: &str) -> Option<HashPrefix> {
-    if prefix.len() > SHA1_BYTE_LENGTH * 2 {
-        return None;
-    }
-
-    let mut data = Vec::new();
-    for index in 0..(prefix.len() / 2) {
-        match u8::from_str_radix(&prefix[2 * index..2 * index + 2], 16) {
-            Ok(value) => data.push(value),
-            Err(_) => return None,
+// The 256 unique strings of length 8 which contain only ' ' and '\t'.
+// These are computed statically in advance to allow them to be copied quickly.
+static PADDINGS: [[u8; 8]; 256] = {
+    let mut paddings = [[0; 8]; 256];
+    let mut i = 0;
+    while i < 256 {
+        let mut j = 0;
+        while j < 8 {
+            paddings[i][j] = if i & (0x80 >> j) == 0 { b' ' } else { b'\t' };
+            j += 1;
         }
+        i += 1;
     }
+    paddings
+};
 
-    Some(HashPrefix {
-        data,
-        half_byte: if prefix.len() % 2 == 1 {
-            match u8::from_str_radix(&prefix[prefix.len() - 1..], 16) {
-                Ok(value) => Some(value << 4),
-                Err(_) => return None,
-            }
-        } else {
-            None
-        },
-    })
-}
-
-fn run_lucky_commit(desired_prefix: &HashPrefix) {
-    let current_commit_bytes = run_command("git", &["cat-file", "commit", "HEAD"]);
-    let current_commit =
-        &String::from_utf8(current_commit_bytes).expect("Git commit contains invalid utf8");
-
-    match find_match(current_commit, desired_prefix) {
-        Some(hash_match) => {
-            create_git_commit(&hash_match)
-                .expect("Found a commit, but failed to write it to the git object database.");
-            git_reset_to_hash(&hash_match.hash);
-        }
-        None => fail_with_message(
-            "Sorry, failed to find a commit matching the givenprefix despite searching over \
-             281 trillion possible commits. Hopefully you haven't just been sitting here \
-             waiting the whole time.",
-        ),
-    }
-}
-
-fn run_command(command: &str, args: &[&str]) -> Vec<u8> {
-    let output = Command::new(command)
-        .args(args)
-        .stderr(Stdio::inherit())
-        .output()
-        .unwrap_or_else(|_| {
-            panic!(
-                "Failed to spawn command `{}` with args `{:?}`",
-                command, args
-            )
-        });
-
-    if !output.status.success() {
-        panic!(
-            "Command finished with non-zero exit code: {} {:?}",
-            command, args
-        );
-    }
-
-    output.stdout
-}
-
-fn find_match(current_commit: &str, desired_prefix: &HashPrefix) -> Option<HashMatch> {
-    let num_threads = num_cpus::get_physical();
-    let (shared_sender, receiver) = mpsc::channel();
-    let counter_ranges = split_range(0, 1u64 << 48, num_threads);
-
-    for counter_range in counter_ranges {
-        spawn_hash_searcher(
-            shared_sender.clone(),
-            SearchParams {
-                current_commit: current_commit.to_owned(),
-                desired_prefix: desired_prefix.clone(),
-                counter_range: counter_range.clone(),
-            },
-        );
-    }
-
-    for _ in 0..num_threads {
-        let result = receiver.recv().unwrap();
-        if result.is_some() {
-            return result;
-        }
-    }
-
-    None
-}
-
-fn spawn_hash_searcher(result_sender: mpsc::Sender<Option<HashMatch>>, params: SearchParams) {
-    thread::spawn(move || {
-        /*
-         * If an error occurs when sending, then the receiver has already received
-         * a match from another thread, so ignore the error.
-         */
-        let _ = result_sender.send(iterate_for_match(&params));
-    });
-}
-
-fn split_range(min: u64, max: u64, num_segments: usize) -> Vec<ops::Range<u64>> {
-    let segment_size = (max - min) / (num_segments as u64);
-
-    let mut segments = Vec::new();
-    let mut last_range_end = 0;
-    for _ in 0..num_segments {
-        segments.push(last_range_end..last_range_end + segment_size);
-        last_range_end += segment_size;
-    }
-
-    segments
-}
-
-fn iterate_for_match(params: &SearchParams) -> Option<HashMatch> {
+pub fn iterate_for_match(params: &SearchParams) -> Option<HashMatch> {
     let desired_prefix = &params.desired_prefix;
     let mut processed_commit = process_commit(&params.current_commit);
 
@@ -328,112 +196,6 @@ fn matches_desired_prefix(hash: &[u8; SHA1_BYTE_LENGTH], prefix: &HashPrefix) ->
         }
 }
 
-fn create_git_commit(search_result: &HashMatch) -> io::Result<()> {
-    assert!(&search_result.raw_object[0..7] == b"commit ");
-    let commit_start_index = search_result
-        .raw_object
-        .iter()
-        .position(|byte| *byte == 0)
-        .expect("No null character found in constructed raw git object?")
-        + 1;
-
-    let mut git_hash_object_child = Command::new("git")
-        .args(&["hash-object", "-t", "commit", "-w", "--stdin"])
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::inherit())
-        .spawn()?;
-
-    git_hash_object_child
-        .stdin
-        .as_mut()
-        .unwrap()
-        .write_all(&search_result.raw_object[commit_start_index..])?;
-    let output = git_hash_object_child.wait_with_output()?;
-
-    if !output.status.success() {
-        panic!("Found a commit, but failed to write it to the git object database.");
-    }
-    let git_hash_output =
-        String::from_utf8(output.stdout).expect("Git produced a hash containing invalid utf8?");
-    assert!(
-        git_hash_output.trim_end() == to_hex_string(&search_result.hash),
-        "Found a commit ({}), but git unexpectedly computed a different hash for it ({})",
-        to_hex_string(&search_result.hash),
-        git_hash_output.trim_end(),
-    );
-    Ok(())
-}
-
-fn git_reset_to_hash(hash: &[u8; SHA1_BYTE_LENGTH]) {
-    run_command("git", &["reset", &to_hex_string(hash)]);
-}
-
-fn to_hex_string(hash: &[u8]) -> String {
-    hash.iter()
-        .map(|byte| format!("{:02x}", *byte))
-        .collect::<String>()
-}
-
-// The 256 unique strings of length 8 which contain only ' ' and '\t'.
-// These are computed statically in advance to allow them to be copied quickly.
-static PADDINGS: [[u8; 8]; 256] = {
-    let mut paddings = [[0; 8]; 256];
-    let mut i = 0;
-    while i < 256 {
-        let mut j = 0;
-        while j < 8 {
-            paddings[i][j] = if i & (0x80 >> j) == 0 { b' ' } else { b'\t' };
-            j += 1;
-        }
-        i += 1;
-    }
-    paddings
-};
-
-fn run_single_core_benchmark() {
-    // Runs a benchmark for performance testing. Using a single core, this does a constant
-    // hash search. This benchmark should take roughly the same amount of time as running
-    // `lucky_commit` with no arguments, but the performance should be much more consistent.
-    // Caveats:
-    // * Assumes perfect parallelization by scaling the workload down when there are multiple
-    //   CPUs. While hash searching is perfectly parallelizable in theory, this benchmark might
-    //   fail to catch performance bugs resulting from contention.
-    // * Assumes that CPU utilization would remain the same when adding threads (this might
-    //   not be the case if the other CPUs are being used for something else)
-    // * Might overestimate the available cache space, since in reality multiple threads would
-    //   be sharing the cache.
-    // * The benchmark uses an unusually long desired prefix to make it implausible that it ends
-    //   early. While this shouldn't result in substantially more instructions executed, in theory
-    //   it could have an effect on things like alignment and the cache.
-    // * The benchmark doesn't spawn any git commands or interact with the filesystem, whereas
-    // * a real run does a ~single-digit number of filesystem operations.
-    //
-    // To use: run `time target/release/lucky_commit --benchmark` and look at the user time.
-    // The observed standard deviation for this benchmark is somewhere around 0.05 seconds.
-    //
-    // For a more end-to-end benchmark without the above caveats and with more noise, see the
-    // `benchmark-branch` branch. The observed standard deviation for that benchmark is somewhere
-    // around 0.5 seconds.
-    assert_eq!(
-        None,
-        iterate_for_match(&SearchParams {
-            current_commit: "\
-                    tree 6f4e79123e206448f80ec73b9a53e07eb0784fef\n\
-                    author Foo Bar <foo@example.com> 1611912738 -0500\n\
-                    committer Foo Bar <foo@example.com> 1611912738 -0500\n\
-                    \n\
-                    Test commit for benchmarking performance changes\n"
-                .to_owned(),
-            desired_prefix: HashPrefix {
-                data: vec![0; 19],
-                half_byte: Some(0x0)
-            },
-            counter_range: 1..((1 << 28) / num_cpus::get_physical() as u64)
-        })
-    );
-}
-
 #[cfg(test)]
 mod tests {
     use std::iter;
@@ -509,101 +271,6 @@ mod tests {
         committer Baz Qux <baz@example.com> 1513980898 -0500\n\
         \n\
         For no particular reason, the commit author's email has a GPG signature marker.\n";
-
-    #[test]
-    fn parse_prefix_empty() {
-        assert_eq!(
-            Some(HashPrefix {
-                data: Vec::new(),
-                half_byte: None
-            }),
-            parse_prefix("")
-        )
-    }
-
-    #[test]
-    fn parse_prefix_single_char() {
-        assert_eq!(
-            Some(HashPrefix {
-                data: Vec::new(),
-                half_byte: Some(0xa0)
-            }),
-            parse_prefix("a")
-        )
-    }
-
-    #[test]
-    fn parse_prefix_even_chars() {
-        assert_eq!(
-            Some(HashPrefix {
-                data: vec![0xab, 0xcd, 0xef],
-                half_byte: None
-            }),
-            parse_prefix("abcdef")
-        )
-    }
-
-    #[test]
-    fn parse_prefix_odd_chars() {
-        assert_eq!(
-            Some(HashPrefix {
-                data: vec![0xab, 0xcd, 0xef],
-                half_byte: Some(0x50)
-            }),
-            parse_prefix("abcdef5")
-        )
-    }
-
-    #[test]
-    fn parse_prefix_capital_letters() {
-        assert_eq!(
-            Some(HashPrefix {
-                data: vec![0xab, 0xcd, 0xef],
-                half_byte: Some(0xb0)
-            }),
-            parse_prefix("ABCDEFB")
-        )
-    }
-
-    #[test]
-    fn parse_prefix_invalid_even_chars() {
-        assert_eq!(None, parse_prefix("abcdgeb"))
-    }
-
-    #[test]
-    fn parse_prefix_invalid_odd_char() {
-        assert_eq!(None, parse_prefix("abcdefg"))
-    }
-
-    #[test]
-    fn parse_prefix_exact_length_match() {
-        assert_eq!(
-            Some(HashPrefix {
-                data: vec![
-                    0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12,
-                    0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78
-                ],
-                half_byte: None
-            }),
-            parse_prefix("1234567812345678123456781234567812345678")
-        )
-    }
-
-    #[test]
-    fn parse_prefix_too_long_with_half_byte() {
-        assert_eq!(
-            None,
-            parse_prefix("12345678123456781234567812345678123456781")
-        )
-    }
-
-    #[test]
-    fn parse_prefix_too_many_full_bytes() {
-        assert_eq!(
-            None,
-            parse_prefix("123456781234567812345678123456781234567812")
-        )
-    }
 
     #[test]
     fn iterate_for_match_failure() {
@@ -878,46 +545,6 @@ mod tests {
     }
 
     #[test]
-    fn split_range_u32_one_segment() {
-        assert_eq!(vec![0..1u64 << 32], split_range(0, 1u64 << 32, 1));
-    }
-
-    #[test]
-    fn split_range_u32_multiple_segments() {
-        let range_max = 1u64 << 32;
-        assert_eq!(
-            vec![
-                0..range_max / 5,
-                range_max / 5..2 * range_max / 5,
-                2 * range_max / 5..3 * range_max / 5,
-                3 * range_max / 5..4 * range_max / 5,
-                4 * range_max / 5..range_max - 1
-            ],
-            split_range(0, range_max, 5)
-        );
-    }
-
-    #[test]
-    fn split_range_u64_one_segment() {
-        assert_eq!(vec![0..u64::MAX], split_range(0, u64::MAX, 1));
-    }
-
-    #[test]
-    fn split_range_u64_multiple_segments() {
-        let range_max = u64::MAX;
-        assert_eq!(
-            vec![
-                0..range_max / 5,
-                range_max / 5..range_max / 5 * 2,
-                range_max / 5 * 2..range_max / 5 * 3,
-                range_max / 5 * 3..range_max / 5 * 4,
-                range_max / 5 * 4..range_max
-            ],
-            split_range(0, range_max, 5)
-        );
-    }
-
-    #[test]
     fn matches_desired_prefix_empty() {
         assert!(matches_desired_prefix(
             &[0; SHA1_BYTE_LENGTH],
@@ -992,15 +619,5 @@ mod tests {
                 half_byte: Some(0x40)
             }
         ))
-    }
-
-    #[test]
-    fn to_hex_string_basic() {
-        assert_eq!("00", to_hex_string(&[0]));
-    }
-
-    #[test]
-    fn to_hex_string_multichar() {
-        assert_eq!("00ff14", to_hex_string(&[0, 255, 20]));
     }
 }
